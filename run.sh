039e44d681427ac33f510c54a1db93fc6a3e31ec
Max Beckenbauer <max.bec92@gmail.com>
2020-11-30 17:20:39 +0100

    Redo
    Signed-off-by: Max Beckenbauer <max.bec92@gmail.com>

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/with-contenv bashio
-
-CONFIG_PATH=/data/options.json
-
-export MAC="$(bashio::config 'mac')"
-export CONFIG_LOCATION="$(bashio::config 'config_path')"
-export DEBUG="$(bashio::config 'debug')"
-
-python3 -u /diyhue/main.py
