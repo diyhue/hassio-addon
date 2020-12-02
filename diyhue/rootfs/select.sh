17eef9ce219dd5d28f75f797adf67bd0c55cef93
Max Beckenbauer <max.bec92@gmail.com>
2020-12-02 14:57:15 +0100

    Entertain Update
    Signed-off-by: Max Beckenbauer <max.bec92@gmail.com>

--- diyhue/rootfs/select.sh
+++ diyhue/rootfs/select.sh
@@ -11,8 +11,19 @@ case "$BUILD_ARCH" in
     *) SELECTED="unsupported" ;;
 esac
 
-echo "Got file suffix: $SELECTED"
-mv ./ssl_server2_diyhue ./entertain-srv
+wget https://github.com/ARMmbed/mbedtls/archive/1ab9b5714852c6810c0a0bfd8c3b5c60a9a15482.zip
+unzip 1ab9b5714852c6810c0a0bfd8c3b5c60a9a15482.zip
+cd mbedtls-1ab9b5714852c6810c0a0bfd8c3b5c60a9a15482/
+wget https://raw.githubusercontent.com/mariusmotea/diyHue/master/BridgeEmulator/ssl_server2_diyhue.c
+make no_test
+gcc -I./include ssl_server2_diyhue.c -o ../entertain-srv -L./library -lmbedtls -lmbedx509 -lmbedcrypto
+cd ..
+
+rm -f 1ab9b5714852c6810c0a0bfd8c3b5c60a9a15482.zip
+rm-f mbedtls-1ab9b5714852c6810c0a0bfd8c3b5c60a9a15482
+
+#echo "Got file suffix: $SELECTED"
+#mv ./ssl_server2_diyhue ./entertain-srv
 mv binaries/coap-client-$SELECTED ./coap-client-linux
 
 # Fix permissions vor hue Entertain
