c46e4d2704f5063c6f7a7cefa9fb1448340ce21c
Max Beckenbauer <max.bec92@gmail.com>
2020-12-28 17:46:56 +0100

    Changed diyhue to dev
    Signed-off-by: Max Beckenbauer <max.bec92@gmail.com>

--- diyhu-beta/rootfs/run.sh
+++ diyhu-beta/rootfs/run.sh
@@ -2,25 +2,12 @@
 
 CONFIG_PATH=/data/options.json
 
-echo "Run Script started..."
-
 export MAC="$(bashio::config 'mac')"
-export CONFIG_LOCATION="$(bashio::config 'config_path')"
+export CONFIG_PATH="$(bashio::config 'config_path')"
 export DEBUG="$(bashio::config 'debug')"
 
 if bashio::config.has_value 'deconz_ip'; then
     export DECONZ="$(bashio::config 'deconz_ip')"
 fi
-
-mkdir -p ${CONFIG_LOCATION}
-
-if [ -f "${CONFIG_LOCATION}/config.json" ]; then
-    echo "${CONFIG_LOCATION}/config.json exists."
-    cp -p ${CONFIG_LOCATION}/config.json /opt/hue-emulator/config.json
-fi
-
-ln -sf /opt/hue-emulator/config.json /config/diyhue/config.json
-
-ls -la
 
 python3 -u /opt/hue-emulator/HueEmulator3.py --docker
