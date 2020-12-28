022374ff87cb851955f17222fc1b265dadf11899
Max Beckenbauer <max.bec92@gmail.com>
2020-12-28 17:36:17 +0100

    Run Update
    Signed-off-by: Max Beckenbauer <max.bec92@gmail.com>

--- diyhu-beta/rootfs/run.sh
+++ diyhu-beta/rootfs/run.sh
@@ -2,6 +2,8 @@
 
 CONFIG_PATH=/data/options.json
 
+echo "Run Script started..."
+
 export MAC="$(bashio::config 'mac')"
 export CONFIG_LOCATION="$(bashio::config 'config_path')"
 export DEBUG="$(bashio::config 'debug')"
@@ -16,6 +18,9 @@ if [ -f "${CONFIG_LOCATION}/config.json" ]; then
     echo "${CONFIG_LOCATION}/config.json exists."
     cp -p ${CONFIG_LOCATION}/config.json /opt/hue-emulator/config.json
 fi
+
 ln -sf /opt/hue-emulator/config.json /config/diyhue/config.json
+
+ls -la
 
 python3 -u /opt/hue-emulator/HueEmulator3.py --docker
