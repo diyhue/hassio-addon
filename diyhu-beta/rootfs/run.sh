0176a4f3ffaa0e89d7173f5db2bc38ff1073ee6c
Max Beckenbauer <max.bec92@gmail.com>
2020-12-28 13:50:41 +0100

    Update
    Signed-off-by: Max Beckenbauer <max.bec92@gmail.com>

--- diyhu-beta/rootfs/run.sh
+++ diyhu-beta/rootfs/run.sh
@@ -11,6 +11,11 @@ if bashio::config.has_value 'deconz_ip'; then
 fi
 
 mkdir -p ${CONFIG_LOCATION}
-ln -s /config/diyhue/config.json /config/diyhue/config.json
+
+if [ -f "${CONFIG_LOCATION}/config.json" ]; then
+    echo "${CONFIG_LOCATION}/config.json exists."
+    cp -p ${CONFIG_LOCATION}/config.json /opt/hue-emulator/config.json
+fi
+ln -sf /opt/hue-emulator/config.json /config/diyhue/config.json
 
 python3 -u /opt/hue-emulator/HueEmulator3.py --docker
