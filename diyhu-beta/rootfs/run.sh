9d8271535ab45d80bb244097cb3883a2e9f83589
Max Beckenbauer <max.bec92@gmail.com>
2020-12-28 13:27:39 +0100

    Update on config_path
    Signed-off-by: Max Beckenbauer <max.bec92@gmail.com>

--- diyhu-beta/rootfs/run.sh
+++ diyhu-beta/rootfs/run.sh
@@ -10,4 +10,7 @@ if bashio::config.has_value 'deconz_ip'; then
     export DECONZ="$(bashio::config 'deconz_ip')"
 fi
 
+mkdir -p ${CONFIG_LOCATION}
+ln -s /config/diyhue/config.json /config/diyhue/config.json
+
 python3 -u /opt/hue-emulator/HueEmulator3.py --docker
