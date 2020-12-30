600c0e6835e19fccb6e28ff0ba5864144fcd5a95
Max Beckenbauer <max.bec92@gmail.com>
2020-12-30 21:08:40 +0100

    Major   update
    Signed-off-by: Max Beckenbauer <max.bec92@gmail.com>

--- diyhue/rootfs/run.sh
+++ diyhue/rootfs/run.sh
@@ -3,11 +3,20 @@
 CONFIG_PATH=/data/options.json
 
 export MAC="$(bashio::config 'mac')"
-export CONFIG_LOCATION="$(bashio::config 'config_path')"
+export CONFIG_PATH="$(bashio::config 'config_path')"
 export DEBUG="$(bashio::config 'debug')"
 
-if bashio::config.has_value 'deconz_ip'; then
+if [[ ! -z "$(bashio::config 'deconz_ip')" ]]; then
     export DECONZ="$(bashio::config 'deconz_ip')"
 fi
+
+if [[ -d $CONFIG_PATH ]]; then
+    echo "$CONFIG_PATH exists."
+else
+    mkdir -p $CONFIG_PATH
+    echo "$CONFIG_PATH created."
+fi
+
+echo "Your Architecture is $BUILD_ARCHI"
 
 python3 -u /opt/hue-emulator/HueEmulator3.py --docker
