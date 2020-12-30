661e03b38e64fdf2c94455462fb22d8a09fcc7e5
Max Beckenbauer <max.bec92@gmail.com>
2020-12-30 10:29:06 +0100

    Implemented entertainment compilation
    Signed-off-by: Max Beckenbauer <max.bec92@gmail.com>

--- diyhu-beta/rootfs/select.sh
+++ diyhu-beta/rootfs/select.sh
@@ -12,31 +12,25 @@ case "$BUILD_ARCH" in
     *) SELECTED="unsupported" ;;
 esac
 
-# wget https://github.com/ARMmbed/mbedtls/archive/1ab9b5714852c6810c0a0bfd8c3b5c60a9a15482.zip
-# unzip 1ab9b5714852c6810c0a0bfd8c3b5c60a9a15482.zip
-# cd mbedtls-1ab9b5714852c6810c0a0bfd8c3b5c60a9a15482/
-# wget https://raw.githubusercontent.com/mariusmotea/diyHue/master/BridgeEmulator/ssl_server2_diyhue.c
-# make no_test
-# gcc -I./include ssl_server2_diyhue.c -o ../entertain-srv -L./library -lmbedtls -lmbedx509 -lmbedcrypto
-# cd ..
+wget https://github.com/ARMmbed/mbedtls/archive/1ab9b5714852c6810c0a0bfd8c3b5c60a9a15482.zip
+unzip 1ab9b5714852c6810c0a0bfd8c3b5c60a9a15482.zip
+cd mbedtls-1ab9b5714852c6810c0a0bfd8c3b5c60a9a15482/
+wget https://raw.githubusercontent.com/mariusmotea/diyHue/master/BridgeEmulator/ssl_server2_diyhue.c
+make no_test
+gcc -I./include ssl_server2_diyhue.c -o ../entertain-srv -L./library -lmbedtls -lmbedx509 -lmbedcrypto
+cd ..
 
-# rm -rf 1ab9b5714852c6810c0a0bfd8c3b5c60a9a15482.zip
-# rm -rf mbedtls-1ab9b5714852c6810c0a0bfd8c3b5c60a9a15482
+rm -rf 1ab9b5714852c6810c0a0bfd8c3b5c60a9a15482.zip
+rm -rf mbedtls-1ab9b5714852c6810c0a0bfd8c3b5c60a9a15482
 
 # echo "Got file suffix: $SELECTED"
-mv /diyhue/BridgeEmulator/entertainment-$SELECTED ./entertain-srv
+#mv /diyhue/BridgeEmulator/entertainment-$SELECTED ./entertain-srv
 mv /diyhue/BridgeEmulator/coap-client-$SELECTED ./coap-client-linux
 
 # Fix permissions vor hue Entertain
 chmod +x ./entertain-srv
 chmod +x ./coap-client-linux
 
-
-# echo "DEBUG OUTPUT:"
-# ldd binaries/entertainment-aarch64
-# echo "DEBUG OUTPUT STOP"
-# md5sum binaries/entertainment-aarch64
-# echo "END OF TEST"
 
 # echo "Files in out folder"
 ls -la
