44eb9244f631ef1ebaace8fb238bbc6fa5677b99
Mark <hendriksen-mark@hotmail.com>
2024-12-01 11:49:49 +0100

    2.0.27
    Add dummy light.
    Add ip setting to config.
    Update wled

--- diyhue/CHANGELOG.md
+++ diyhue/CHANGELOG.md
@@ -2,6 +2,16 @@
 
 All notable changes to this project will be documented in this file. The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [2.0.27 - 1.12.2024]
+### add
+    - Add dummy
+    - Add ip setting to config
+### update
+    - update wled
+### info
+    - see main repo for more info
+    - disable backup if you get a error "Unsuccessful websocket message"
+
 ## [2.0.26 - 24.11.2024]
 ### add
     - Add effect_v2
--- diyhue/rootfs/run.sh
+++ diyhue/rootfs/run.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/with-contenv bashio
 
 CONFIG_PATH=/data/options.json
+IP=""
 
 export MAC="$(bashio::config 'mac')"
 export CONFIG_PATH="$(bashio::config 'config_path')"
 export DEBUG="$(bashio::config 'debug')"
 export HTTP_PORT="$(bashio::config 'http_port')"
 export HTTPS_PORT="$(bashio::config 'https_port')"
+export IP="$(bashio::config 'ip')"
 
 
 export NO_SERVE_HTTPS="$(bashio::config 'no_serve_https')"
@@ -23,8 +25,8 @@ echo "Your Architecture is $BUILD_ARCHI"
 
 if [ "$NO_SERVE_HTTPS" = "true" ] ; then
     echo "No serve HTTPS"
-    python3 -u /opt/hue-emulator/HueEmulator3.py --docker --no-serve-https
+    python3 -u /opt/hue-emulator/HueEmulator3.py --docker --no-serve-https --ip "$IP"
 else
     echo "Serve HTTPS"
-    python3 -u /opt/hue-emulator/HueEmulator3.py --docker
+    python3 -u /opt/hue-emulator/HueEmulator3.py --docker --ip "$IP"
 fi
